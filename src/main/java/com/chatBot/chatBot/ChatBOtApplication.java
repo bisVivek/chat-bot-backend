efe6de1e8d2e112a5b2239d45820c5a90024361d
bisVivek <vivek5832017@gmail.com>
2024-12-31 16:36:48 +0530

    initial commit

---
+++ src/main/java/com/chatBot/chatBot/ChatBOtApplication.java
@@ -0,0 +1,13 @@
+package com.chatBot.chatBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBOtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBOtApplication.class, args);
+	}
+
+}
